c54f26539996dd4c58ce5c405886ee6647095b04
Venkat0406-aws <venkatraoinala120@gmail.com>
2024-08-18 07:28:31 +0530

    Update cloneRepo.groovy

--- vars/cloneRepo.groovy
+++ vars/cloneRepo.groovy
@@ -2,7 +2,7 @@ def call(String repoUrl, String branch = 'main', String credentialsId = '') {
     checkout([
         $class: 'GitSCM',
         branches: [[name: "*/${main}"]],
-        userRemoteConfigs: [[url: https://github.com/Venkat0406-aws/Maven.git, credentialsId: Git]]
+        userRemoteConfigs: [[url: 'https://github.com/Venkat0406-aws/Maven.git', credentialsId: 'Git']]
     ])
 }
 
