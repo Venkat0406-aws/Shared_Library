ff3a490504202ba28df1ce6656a7d9cd26b065a7
root <root@ip-172-31-34-161.ap-south-1.compute.internal>
2024-08-18 01:39:44 +0000

    cloneRepo.groovy

---
+++ vars/cloneRepo.groovy
@@ -0,0 +1,8 @@
+def call(String repoUrl, String branch = 'main', String credentialsId = '') {
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        userRemoteConfigs: [[url: repoUrl, credentialsId: credentialsId]]
+    ])
+}
+
